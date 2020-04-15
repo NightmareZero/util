29ce31130e9d25131aa66303acf99127916bb50f
night <zhuxy@irisking.com>
2020-04-15 15:10:33 +0800

    time convert py remove old files

--- timeConvertPy/timeToStr.py
+++
@@ -1,7 +0,0 @@
-import time
-from time import struct_time
-
-timeArray: struct_time = time.localtime(1579449603150 / 1000)
-timeStr: str = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-
-print(timeStr)
